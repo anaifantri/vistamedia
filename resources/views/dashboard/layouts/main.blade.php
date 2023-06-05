e8a1c0201e4e1ae7814b040d724dc2c56b8b90ee
anaifantri <ana.ifantri@gmail.com>
2023-06-05 15:24:40 +0800

    Finishing build css tailwind

--- resources/views/dashboard/layouts/main.blade.php
+++ resources/views/dashboard/layouts/main.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @vite('resources/css/app.css')
+    <link rel="stylesheet" href="/css/style.css">
     <title>VISTA MEDIA | {{ $title }}</title>
     <link rel="icon" href="/img/favicon.ico" type="image/x-icon">
 </head>
